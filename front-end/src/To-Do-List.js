aef9006207bf7e77623d244c946c7c2c46ceda9c
abdelkarim <skwyz01@outlook.com>
2023-01-12 18:51:54 +0100

    add : functions call in front-end

--- front-end/src/To-Do-List.js
+++ front-end/src/To-Do-List.js
@@ -16,6 +16,96 @@ class ToDoList extends Component {
     ComponentDidMount(){
         this.getTask();
     }
+    onChange = (event) => {
+        this.SetState({
+            [event.target.name] : event.targe.value,
+        })
+    }
+
+    onSubmit
+
+    getTask = ()=> {
+        axios.get(endpoint + ("/api/task").then((result)=> {
+            if (res.data) {
+                this.SetState({
+                    items: res.data.map((item)=>{
+                        let color = "yellow";
+                        let style = {
+                            wordWrap: "break-word",
+                        };
+
+                        if (item.status){
+                            color="green";
+                            style["textDecorationLine"] = "line-through";
+                        }
+
+                        return (
+                           <Card key={item.id} color={color} fluid className="rough">
+                                <Card.Content>
+                                    <Card.Header textAlign="left">
+                                        <div style={style}>{item.task}</div>
+                                    </Card.Header>
+                                        <Card.Meta textAlign="right">
+                                            <Icon
+                                            name="check circle"
+                                            color="blue"
+                                            onClick={() => this.updateTask(item.id)}
+                                            />
+                                            <span style={{paddingRight: 10}}>Undo</span>
+                                            <Icon
+                                            name="delete"
+                                            color="red"
+                                            onClick={()=> this.deleteTask(task.id)}
+                                            />
+                                            <span style={{paddingRight: 10}}>Delete</span>
+                                        </Card.Meta> 
+                                </Card.Content>
+                           </Card> 
+                        )
+                    })
+                })
+            } else {
+                this.SetState({
+                    items:[],
+                });
+            }
+        }));
+    };
+ 
+    updateTask = (id) => {
+        axios.put(endpoint + "/api/task/" + id, {
+            headers:{
+                "Content-Type":"application/x-www-form-urlencoded",
+            },
+        }).then((res)=>{
+            console.log(res);
+            this.getTask();
+        })
+    }
+
+    undoTask = (id) => {
+        axios.put(endpoint + "api/task/" + id, {
+            headers:{
+                "Content-Type":"application/x-www-form-urlencoded",
+            },
+        }).then((res)=>{
+            console.log(res)
+            this.getTask();
+        })
+    }
+
+    deleteTask = (id) => {
+        axios.delete(endpoint + "api/deleteTask/" + id, {
+            headers:{
+                "Content-Type":"application/x-www-form-urlencoded",
+            },
+        }).then((res)=>{
+            console.log(res)
+            this.getTask();
+        })
+    }
+
+
 
     render(){
         return(
@@ -36,8 +126,12 @@ class ToDoList extends Component {
                         fluid
                         placeholder="Create Task"
                         />
+                    {/* {<Button> Create Task </Button>} */}
                     </Form>
-                </div>
+                    </div>
+                    <div className="row">
+                        <Card.Group> {this.state.item} </Card.Group>
+                    </div>
             </div>
         );
     }
